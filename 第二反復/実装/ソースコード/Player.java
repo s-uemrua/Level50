d6fa5ec75a09eb49c45cc8002d7a85044d6f00f2
Yamamoto Kiri <kir-yamamoto@cresco.co.jp>
2020-06-11 12:22:39 +0900

    追加

---
+++ 第二反復/実装/ソースコード/Player.java
@@ -0,0 +1,22 @@
+package ミニプロジェクト2;
+
+abstract  class Player {
+	//先攻、後攻をいれる。
+	private String order;
+	private String mark;
+	
+	//プレイヤー名（先攻、後攻）のセット
+	public Player(String order,String mark) {
+		this.order = order;
+		this.mark = mark;
+	}
+	
+	abstract int[] inputCoordinate();
+	
+	public  String getOrder() {
+		return order;
+	}
+	public String getMark() {
+		return mark;
+	}
+}
